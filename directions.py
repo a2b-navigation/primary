93d315d8da499698858b33e580333fa6b3efb1ab
Luke <11898833+curlpipe@users.noreply.github.com>
2024-04-08 19:43:21 +0100

    fixed stupid mapbox api nonsense

--- directions.py
+++ directions.py
@@ -5,9 +5,12 @@ access_token = "pk.eyJ1IjoibWFnbmV0aWNub3J0aCIsImEiOiJjbG02OTdocW8xdzBlM3BzNm96b
 
 # Given starting and ending coordinates, generate beacons to go from start to end
 def beacons(start, end):
+    start = ",".join(start.replace(", ", ",").split(",")[::-1])
+    end = ",".join(end.replace(", ", ",").split(",")[::-1])
     print("[Route Creation] Sending request to mapbox...")
-    raw = r.get(f"https://api.mapbox.com/directions/v5/mapbox/cycling/{start};{end}?geometries=geojson&access_token={access_token}")
+    raw = r.get(f"https://api.mapbox.com/directions/v5/mapbox/cycling/{start};{end}?geometries=geojson&steps=true&access_token={access_token}")
     data = json.loads(raw.text)
+    print(data)
     
     # Obtain beacons
     print("[Route Creation] Generating beacons...")
