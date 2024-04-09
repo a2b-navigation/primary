3e5cdfe66fb6a2e0a0b2ec92d2d14a9bb34740a3
Luke <11898833+curlpipe@users.noreply.github.com>
2024-04-09 12:08:10 +0100

    fixed tuple issue

--- primary.py
+++ primary.py
@@ -278,6 +278,6 @@ def error(msg):
 
 if __name__ == "__main__":
     open_command_centre()
-    server = mp.Process(target=app.run, args=(host="0.0.0.0",))
+    server = mp.Process(target=app.run, args=("0.0.0.0",))
     server.start()
 
