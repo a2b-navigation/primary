eb4fd50122fac736dc0d04b4a823eba81362cea4
Luke <11898833+curlpipe@users.noreply.github.com>
2024-04-11 17:40:33 +0100

    adjusted actuation patterns

--- actuation.py
+++ actuation.py
@@ -9,11 +9,11 @@ def burst(length):
 # This will actuate a device n_vibrations per second
 def actuate(n_vibrations):
     time_between = 1 / n_vibrations / 2
-    for _ in range(int(n_vibrations)):
+    for _ in range(max(1, round(n_vibrations))):
         burst(time_between)
         t.sleep(time_between)
 
-very_near = lambda: actuate(1)
-near = lambda: actuate(2)
-far = lambda: actuate(4)
-very_far = lambda: actuate(8)
+very_near = lambda: actuate(0.5)
+near = lambda: actuate(1)
+far = lambda: actuate(3)
+very_far = lambda: actuate(5)
